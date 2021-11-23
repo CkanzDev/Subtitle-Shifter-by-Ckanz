cea6fbcd498e40a3f94133051fd2d99a00ffd6d4
Ckanz <48119731+CkanzDev@users.noreply.github.com>
2021-11-23 22:14:00 +0700

    Add project files.

---
+++ Subtitle Sync Shifter by Ckanz/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace Subtitle_Sync_Shifter_by_Ckanz
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new System.Windows.Forms.Label();
+            this.loadclik = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.increaseclick = new System.Windows.Forms.Button();
+            this.syncdecreases = new System.Windows.Forms.Button();
+            this.Saveclicks = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.openfiledialog = new System.Windows.Forms.OpenFileDialog();
+            this.savefiledialog = new System.Windows.Forms.SaveFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.testbutton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 3);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Created by Ckanz";
+            // 
+            // loadclik
+            // 
+            this.loadclik.Location = new System.Drawing.Point(234, 296);
+            this.loadclik.Name = "loadclik";
+            this.loadclik.Size = new System.Drawing.Size(140, 59);
+            this.loadclik.TabIndex = 1;
+            this.loadclik.Text = "Load";
+            this.loadclik.UseVisualStyleBackColor = true;
+            this.loadclik.Click += new System.EventHandler(this.loadclik_Click_1);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(234, 35);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox3.Size = new System.Drawing.Size(581, 255);
+            this.textBox3.TabIndex = 2;
+            // 
+            // increaseclick
+            // 
+            this.increaseclick.Location = new System.Drawing.Point(383, 296);
+            this.increaseclick.Name = "increaseclick";
+            this.increaseclick.Size = new System.Drawing.Size(140, 59);
+            this.increaseclick.TabIndex = 3;
+            this.increaseclick.Text = "Sync Increse";
+            this.increaseclick.UseVisualStyleBackColor = true;
+            this.increaseclick.Click += new System.EventHandler(this.increaseclick_Click);
+            // 
+            // syncdecreases
+            // 
+            this.syncdecreases.Location = new System.Drawing.Point(529, 296);
+            this.syncdecreases.Name = "syncdecreases";
+            this.syncdecreases.Size = new System.Drawing.Size(140, 59);
+            this.syncdecreases.TabIndex = 4;
+            this.syncdecreases.Text = "Sync Decrease";
+            this.syncdecreases.UseVisualStyleBackColor = true;
+            this.syncdecreases.Click += new System.EventHandler(this.syncdecreases_Click);
+            // 
+            // Saveclicks
+            // 
+            this.Saveclicks.Location = new System.Drawing.Point(675, 296);
+            this.Saveclicks.Name = "Saveclicks";
+            this.Saveclicks.Size = new System.Drawing.Size(140, 59);
+            this.Saveclicks.TabIndex = 5;
+            this.Saveclicks.Text = "Save";
+            this.Saveclicks.UseVisualStyleBackColor = true;
+            this.Saveclicks.Click += new System.EventHandler(this.Saveclicks_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(234, 368);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ShortcutsEnabled = false;
+            this.textBox4.Size = new System.Drawing.Size(581, 20);
+            this.textBox4.TabIndex = 6;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged_1);
+            // 
+            // openfiledialog
+            // 
+            this.openfiledialog.FileName = "openFileDialog1";
+            this.openfiledialog.Filter = "srt|*.srt";
+            this.openfiledialog.Title = "Choose a file to import";
+            this.openfiledialog.FileOk += new System.ComponentModel.CancelEventHandler(this.openfiledialog_FileOk);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(240, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 7;
+            // 
+            // testbutton
+            // 
+            this.testbutton.Location = new System.Drawing.Point(740, 6);
+            this.testbutton.Name = "testbutton";
+            this.testbutton.Size = new System.Drawing.Size(75, 23);
+            this.testbutton.TabIndex = 8;
+            this.testbutton.Text = "test";
+            this.testbutton.UseVisualStyleBackColor = true;
+            this.testbutton.Click += new System.EventHandler(this.testbutton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(131, 371);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Miliseconds(Ms) -->";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(827, 400);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.testbutton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.Saveclicks);
+            this.Controls.Add(this.syncdecreases);
+            this.Controls.Add(this.increaseclick);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.loadclik);
+            this.Controls.Add(this.label3);
+            this.Name = "Form1";
+            this.Text = "Subtitle Shifter Ckanz";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button loadclik;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button increaseclick;
+        private System.Windows.Forms.Button syncdecreases;
+        private System.Windows.Forms.Button Saveclicks;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.OpenFileDialog openfiledialog;
+        private System.Windows.Forms.SaveFileDialog savefiledialog;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button testbutton;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ Subtitle Sync Shifter by Ckanz/Form1.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Subtitle_Sync_Shifter_by_Ckanz
+{
+    public partial class Form1 : Form
+    {
+        public double miliseconds;
+        public List<String> line = new List<string>();
+        public List<String> newLine = new List<string>();
+        public TimeSpan time = TimeSpan.FromMilliseconds(0);
+        string text = "";
+        static Dictionary<string, string> values = new Dictionary<string, string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            textBox4.KeyPress += new KeyPressEventHandler(keypressed);
+        }
+
+        private void loadclik_Click_1(object sender, EventArgs e)
+        {
+            if (openfiledialog.ShowDialog() == DialogResult.OK)
+            newLine.Clear();
+            line.Clear();
+            values.Clear();
+            foreach (String f in openfiledialog.FileNames)
+            {
+                text = File.ReadAllText(f);
+                //textBox1.Text += f + "\r\n";
+                foreach (string l in File.ReadAllLines(f))
+                {
+                    if (Regex.IsMatch(l, " --> "))
+                    {
+                        string[] determites = { " --> ", "-->" };
+                        string[] i = l.Split(determites, StringSplitOptions.RemoveEmptyEntries);
+                        line.Add(i[i.Count() - 2]);
+                        line.Add(i[i.Count() - 1]);
+                    }
+                }
+                //foreach (var line in File.ReadLines(f))
+                //{
+                //if (Regex.IsMatch(line, " --> "))
+                //{
+                //Console.Write("Ketemu" + line.Length);
+                //string[] determites = { " --> ", "-->" };
+                //lines = line.Split(determites, StringSplitOptions.RemoveEmptyEntries);
+                //int result = Int32.Parse(lines[0]);
+                //textBox1.Text += "Jumlah:" + lines.Count() + " Ketemu : " + lines[lines.Count() - 1] + "\r\n";
+                //}
+                //textBox3.Text += text;
+            }
+            //foreach (string l in line) textBox3.Text += l + "\r\n";
+            label1.Text = "Done! Fill Miliseconds textbox and click button sync decrese or increse";
+        }
+
+        private void increaseclick_Click(object sender, EventArgs e)
+        {
+            if (miliseconds <= 0 || textBox4.Text == null || textBox4.Text == "") label1.Text = "miliseconds cant null or less than 1";
+            label1.Text = "Wait...";
+            textBox3.Text = "";
+            foreach (string l in line)
+            {
+                //string newText = "";
+
+                if (l.ToCharArray().Count() <= 12)
+                {
+                    string oldText = (0 + Convert.ToInt32(l[l.Count() - 12].ToString())).ToString() + (0 + Convert.ToInt32(l[l.Count() - 11].ToString())).ToString() + ":"
+                    + (0 + Convert.ToInt32(l[l.Count() - 9].ToString())).ToString() + (0 + Convert.ToInt32(l[l.Count() - 8].ToString())).ToString() + ":"
+                    + (0 + Convert.ToInt32(l[l.Count() - 6].ToString())).ToString() + (0 + Convert.ToInt32(l[l.Count() - 5].ToString())).ToString() + "."
+                    + (0 + Convert.ToInt32(l[l.Count() - 3].ToString())).ToString() + (0 + Convert.ToInt32(l[l.Count() - 2].ToString())).ToString() + (0 + Convert.ToInt32(l[l.Count() - 1].ToString())).ToString();
+
+                    //string oint = (Convert.ToInt32(oldText) + miliseconds).ToString();
+                    TimeSpan ointTime = TimeSpan.Parse(oldText);
+                    TimeSpan times = TimeSpan.FromMilliseconds(miliseconds);
+                    TimeSpan oTime = ointTime.Add(times);
+                    string oint = oTime.ToString();
+                    char[] charsToTrim = { ':', '.' };
+                    string oints = oint.Replace(".", ",").Remove(oint.Length - 4).ToString();
+                    //textBox3.Text += oints + "\r\n";
+                    //textBox3.Text += oint[oint.Count() - 1] + "\r\n";
+                    //textBox3.Text += oint + "\r\n";
+                    //textBox3.Text += oint.ToCharArray().Count() + "\r\n";
+                    //textBox3.Text += "9" + "\r\n";
+                    //newText = (0 + Convert.ToInt32(oint[oint.Count() - 9].ToString())).ToString() + (0 + Convert.ToInt32(oint[oint.Count() - 8].ToString())).ToString() + ":"
+                    //+ (0 + Convert.ToInt32(oint[oint.Count() - 7].ToString())).ToString() + (0 + Convert.ToInt32(oint[oint.Count() - 6].ToString())).ToString() + ":"
+                    //+ (0 + Convert.ToInt32(oint[oint.Count() - 5].ToString())).ToString() + (0 + Convert.ToInt32(oint[oint.Count() - 4].ToString())).ToString() + ","
+                    //+ (0 + Convert.ToInt32(oint[oint.Count() - 3].ToString())).ToString() + (0 + Convert.ToInt32(oint[oint.Count() - 2].ToString())).ToString() + (0 + Convert.ToInt32(l[l.Count() - 1].ToString())).ToString();
+                    //newText = (0 + Convert.ToInt32(oints[0])).ToString() + (0 + Convert.ToInt32(oints[1])).ToString() + ":"
+                    //+ (0 + Convert.ToInt32(oints[2])).ToString() + (0 + Convert.ToInt32(oints[3])).ToString() + ":"
+                    //+ (0 + Convert.ToInt32(oints[4])).ToString() + (0 + Convert.ToInt32(oints[5])).ToString() + ","
+                    //+ (0 + Convert.ToInt32(oints[6])).ToString() + (0 + Convert.ToInt32(oints[7])).ToString() + (0 + Convert.ToInt32(oints[8])).ToString();
+                    newLine.Add(oints);
+                    for (var v = 0; v < newLine.Count() - 1; v++)
+                    {
+                        if(!values.ContainsKey(newLine[v]) || !values.ContainsKey(newLine[v]))
+                        {
+                            values.Add(newLine[v], line[v]);
+                        }
+                    }
+
+
+                    //textBox3.Text += oints + "\r\n";
+                    label1.Text = "Done! click save";
+
+                }
+            }
+        }
+
+        private void textBox4_TextChanged_1(object sender, EventArgs e)
+        {
+            if (System.Text.RegularExpressions.Regex.IsMatch(textBox4.Text, "[^0-9]"))
+            {
+                MessageBox.Show("Please enter only numbers.");
+                textBox4.Text = textBox4.Text.Remove(textBox4.Text.Length - 1);
+                
+            }
+           
+        }
+
+        private void keypressed(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) &&
+                (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+                
+            }
+
+            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void syncdecreases_Click(object sender, EventArgs e)
+        {
+            if (miliseconds <= 0 || textBox4.Text == null || textBox4.Text == "") label1.Text = "miliseconds cant null or less than 1";
+            label1.Text = "Wait...";
+            textBox3.Text = "";
+            foreach (string l in line)
+            {
+                //string newText = "";
+
+                if (l.ToCharArray().Count() <= 12)
+                {
+                    string oldText = (0 + Convert.ToInt32(l[l.Count() - 12].ToString())).ToString() + (0 + Convert.ToInt32(l[l.Count() - 11].ToString())).ToString() + ":"
+                    + (0 + Convert.ToInt32(l[l.Count() - 9].ToString())).ToString() + (0 + Convert.ToInt32(l[l.Count() - 8].ToString())).ToString() + ":"
+                    + (0 + Convert.ToInt32(l[l.Count() - 6].ToString())).ToString() + (0 + Convert.ToInt32(l[l.Count() - 5].ToString())).ToString() + "."
+                    + (0 + Convert.ToInt32(l[l.Count() - 3].ToString())).ToString() + (0 + Convert.ToInt32(l[l.Count() - 2].ToString())).ToString() + (0 + Convert.ToInt32(l[l.Count() - 1].ToString())).ToString();
+
+                    //string oint = (Convert.ToInt32(oldText) + miliseconds).ToString();
+                    TimeSpan ointTime = TimeSpan.Parse(oldText);
+                    TimeSpan times = TimeSpan.FromMilliseconds(miliseconds);
+                    TimeSpan oTime = ointTime.Subtract(times);
+                    string oint = oTime.ToString();
+                    char[] charsToTrim = { ':', '.' };
+                    string oints = oint.Replace(".",",").Remove(oint.Length - 4).ToString();
+                    //textBox3.Text += oints + "\r\n";
+                    //textBox3.Text += oint[oint.Count() - 1] + "\r\n";
+                    //textBox3.Text += oint + "\r\n";
+                    //textBox3.Text += oint.ToCharArray().Count() + "\r\n";
+                    //textBox3.Text += "9" + "\r\n";
+                    //newText = (0 + Convert.ToInt32(oint[oint.Count() - 9].ToString())).ToString() + (0 + Convert.ToInt32(oint[oint.Count() - 8].ToString())).ToString() + ":"
+                    //+ (0 + Convert.ToInt32(oint[oint.Count() - 7].ToString())).ToString() + (0 + Convert.ToInt32(oint[oint.Count() - 6].ToString())).ToString() + ":"
+                    //+ (0 + Convert.ToInt32(oint[oint.Count() - 5].ToString())).ToString() + (0 + Convert.ToInt32(oint[oint.Count() - 4].ToString())).ToString() + ","
+                    //+ (0 + Convert.ToInt32(oint[oint.Count() - 3].ToString())).ToString() + (0 + Convert.ToInt32(oint[oint.Count() - 2].ToString())).ToString() + (0 + Convert.ToInt32(l[l.Count() - 1].ToString())).ToString();
+                    //newText = (0 + Convert.ToInt32(oints[0])).ToString() + (0 + Convert.ToInt32(oints[1])).ToString() + ":"
+                    //+ (0 + Convert.ToInt32(oints[2])).ToString() + (0 + Convert.ToInt32(oints[3])).ToString() + ":"
+                    //+ (0 + Convert.ToInt32(oints[4])).ToString() + (0 + Convert.ToInt32(oints[5])).ToString() + ","
+                    //+ (0 + Convert.ToInt32(oints[6])).ToString() + (0 + Convert.ToInt32(oints[7])).ToString() + (0 + Convert.ToInt32(oints[8])).ToString();
+                    newLine.Add(oints);
+                    for (var v = 0; v < newLine.Count() - 1; v++)
+                    {
+                        if (!values.ContainsKey(newLine[v]) || !values.ContainsKey(newLine[v]))
+                        {
+                            values.Add(newLine[v], line[v]);
+                        }
+                    }
+
+                    //textBox3.Text += ls + "\r\n";
+                    label1.Text = "Done! click save";
+
+                }
+            }
+        }
+
+        private void Saveclicks_Click(object sender, EventArgs e)
+        {
+                foreach (String f in openfiledialog.FileNames)
+                {
+                //textBox1.Text += f + "\r\n";
+
+                //StreamReader streamReader = new StreamReader(f, Encoding.UTF8);
+                //string text = streamReader.ReadToEnd();
+                string fs = Path.Combine("C:\\scraped.txt");
+                string results = text;
+                StringBuilder sv = new StringBuilder(text);
+                //foreach (string l in File.ReadAllLines(f))
+                //{
+                 //for (int i = 0; i < newLine.Count(); i++)
+                   //{
+                     //Regex.Replace(text, line[i], newLine[i], RegexOptions.IgnoreCase);
+                     //sv = sv.Replace(line[i], newLine[i]);
+                     //results = sv.ToString();
+                    //}
+                 //}
+                foreach (string k in values.Keys)
+                {
+                    text.Replace(k, values[k]);
+                }
+                //const int BufferSize = 65536;
+                //StreamWriter sw = new StreamWriter(fs, true, Encoding.UTF8, BufferSize);
+                //TextWriter w = new StreamWriter(new BufferedStream(new FileStream(fs, FileMode.Create)));
+                //w.WriteLine(results);
+                using (var stream = new FileStream(
+    fs, FileMode.Create, FileAccess.Write, FileShare.Write, 4096))
+                {
+                    var bytes = Encoding.UTF8.GetBytes(results);
+                    stream.Write(bytes, 0, bytes.Length);
+                }
+                //w.Write(results);
+                //File.WriteAllText(fs, results);
+                label1.Text = "Done File In : " + fs.ToString();
+
+                results = "";
+                sv.Clear();
+                text = "";
+                newLine.Clear();
+                line.Clear();
+                values.Clear();
+
+            }
+            
+            
+        }
+
+        private void testbutton_Click(object sender, EventArgs e)
+        {
+        
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("This Application Still In Development, many bugs and slow");
+        }
+
+        private void openfiledialog_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+    }
+    }
